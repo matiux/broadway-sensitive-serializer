949280a3ffb3bc616b8deea568872da6feea1fe0
Matteo <m.galacci@gmail.com>
2021-11-25 18:43:03 +0100

    feat: preserves payload keys in WholePayloadSensitizer

--- tests/Integration/SensitiveSerializer/Serializer/Strategy/WholePayloadStrategy/WholePayloadSensitizerTestUtil.php
+++ tests/Integration/SensitiveSerializer/Serializer/Strategy/WholePayloadStrategy/WholePayloadSensitizerTestUtil.php
@@ -5,16 +5,25 @@ declare(strict_types=1);
 namespace Tests\Integration\SensitiveSerializer\Serializer\Strategy\WholePayloadStrategy;
 
 use Matiux\Broadway\SensitiveSerializer\DataManager\Domain\Exception\AggregateKeyException;
+use Matiux\Broadway\SensitiveSerializer\DataManager\Domain\Service\SensitiveTool;
 
 trait WholePayloadSensitizerTestUtil
 {
+    /**
+     * @param array{class: class-string, payload: array<string, string>} $sensitizedOutgoingPayload
+     */
     private static function assertObjectIsSensitized(array $sensitizedOutgoingPayload): void
     {
         self::assertArrayHasKey('class', $sensitizedOutgoingPayload);
         self::assertArrayHasKey('payload', $sensitizedOutgoingPayload);
-        self::assertIsArray($sensitizedOutgoingPayload['payload']);
         self::assertArrayHasKey('id', $sensitizedOutgoingPayload['payload']);
-        self::assertArrayHasKey('sensible_data', $sensitizedOutgoingPayload['payload']);
+
+        $sensitizedData = $sensitizedOutgoingPayload['payload'];
+        unset($sensitizedData['id']);
+
+        foreach ($sensitizedData as $sensitizedValue) {
+            self::assertTrue(SensitiveTool::isSensitized($sensitizedValue));
+        }
     }
 
     /**
@@ -29,11 +38,14 @@ trait WholePayloadSensitizerTestUtil
         $expectedPayload = (array) $this->ingoingPayload['payload'];
         unset($expectedPayload['id']);
 
-        $decodedPayload = json_decode(
-            $this->sensitiveDataManager->decrypt($sensitizedOutgoingPayload['payload']['sensible_data'], $decryptedAggregateKey),
-            true
-        );
+        $sensitizedData = $sensitizedOutgoingPayload['payload'];
+        unset($sensitizedData['id']);
 
-        self::assertSame($expectedPayload, $decodedPayload);
+        $desensitizedValues = [];
+        foreach ($sensitizedData as $key => $sensitizedValue) {
+            $desensitizedValues[$key] = $this->sensitiveDataManager->decrypt($sensitizedValue, $decryptedAggregateKey);
+        }
+
+        self::assertSame($expectedPayload, $desensitizedValues);
     }
 }
